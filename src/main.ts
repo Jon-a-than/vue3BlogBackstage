79c5d837faeed8a16e164a8cd802bea45c0bd5d0
Jon-a-than <2140145186@qq.com>
2022-04-27 12:57:02 +0800

    feat: add iconfont

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { router } from '@/router'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import './assets/fonts/iconfont.css'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 
 app.mount('#app')
