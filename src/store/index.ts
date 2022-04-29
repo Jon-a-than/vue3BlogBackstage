5c5e727a613574d05d796810effe3bb0565015c8
Jon-a-than <2140145186@qq.com>
2022-04-29 16:40:41 +0800

    chore: add pinia-plugin-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,5 @@
 import { createPinia } from 'pinia'
+import piniaPluginPersist from 'pinia-plugin-persist'
+export { useUserStore } from './modules/user'
+
+export default createPinia().use(piniaPluginPersist)
