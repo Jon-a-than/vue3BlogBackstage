d5abd5ea1bca42f77b0449fef24cb7eddad5ed7e
Jon-a-than <2140145186@qq.com>
2022-05-01 13:41:32 +0800

    feat: add v-md-editor

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,46 @@ import '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import '@/assets/fonts'
 
+import VMdEditor from '@kangc/v-md-editor/lib/codemirror-editor';
+import '@kangc/v-md-editor/lib/style/codemirror-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// highlightjs
+import hljs from 'highlight.js';
+
+// codemirror 编辑器的相关资源
+import Codemirror from 'codemirror';
+// mode
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/vue/vue';
+// edit
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/matchbrackets';
+// placeholder
+import 'codemirror/addon/display/placeholder';
+// active-line
+import 'codemirror/addon/selection/active-line';
+// scrollbar
+import 'codemirror/addon/scroll/simplescrollbars';
+import 'codemirror/addon/scroll/simplescrollbars.css';
+// style
+import 'codemirror/lib/codemirror.css';
+
+VMdEditor.Codemirror = Codemirror;
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+});
+
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
 app.use(pinia)
+app.use(VMdEditor)
 
 app.mount('#app')
--- src/views/creator/index.vue
+++ src/views/creator/index.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
-  
+import { ref } from 'vue';
+
+const article = ref('')
+
 </script>
 
 <template>
   Creator
+  <v-md-editor v-model="article" height="400px"></v-md-editor>
 </template>
 
 <style lang="scss" scoped>
