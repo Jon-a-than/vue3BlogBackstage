b9364f2f40a417d8f214695864a942a6ab9798b5
Jon-a-than <2140145186@qq.com>
2022-05-02 21:25:22 +0800

    feat: set timeout

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.timeout = 5000
 
 const ApiGet = (url: string, params?: any, config?: any) => {
   return axios.get(url, { params, ...config })
